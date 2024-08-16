f5b7d05e96c036f78d4568e187632dc462bd92f4
Pete Cooper <pete@pragmatika.net>
2024-08-16 20:14:34 +0100

    first pass of well-known-rebuild

--- www/shared/scripts/well-known-rebuild.sh
+++ www/shared/scripts/well-known-rebuild.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 mkdir -p ../_well-known/.well-known/acme-challenge/
+curl -Lo ../_well-known/index.html https://raw.githubusercontent.com/petecooper/cloud-server-conf/main/www/servers/-/_well-known/index.html
+curl -Lo ../_well-known/.well-known/index.html https://raw.githubusercontent.com/petecooper/cloud-server-conf/main/www/servers/-/_well-known/.well-known/index.html
+curl -Lo ../_well-known/.well-known/acme-challenge/index.html https://raw.githubusercontent.com/petecooper/cloud-server-conf/main/www/servers/-/_well-known/.well-known/acme-challenge/index.html
 chown -R root:sudo ../_well-known/
 chmod -R 775 ../_well-known/
